d1eceecaed47ded2b314574ac34a2a1a39275407
Tony <Tony@192.168.2.2>
2021-05-14 15:13:14 -0400

    Day 4 Junit Testing

--- JavaBasics/tests/com/ss/jb/four/LineTest.java
+++ JavaBasics/tests/com/ss/jb/four/LineTest.java
@@ -2,29 +2,42 @@ package com.ss.jb.four;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class LineTest {
 	Line l = new Line(1, 2, 3, 4);
+	Line l2 = new Line(3.5, 2.8, 3.5, 9.1);
+	
 	@Test
 	public void getSlopeTest() {
+		assertEquals(1, l.getSlope(), .0001);
+		assertNotEquals(10.5, l.getSlope(), .0001);
 		
-		assertEquals(2.8284, l.getSlope(), .0001);
+	}
+	
+	@Rule
+	public ExpectedException ex = ExpectedException.none();
+	@Test
+	public void getSlopeExceptionTest() {
 		
+		ex.expect(ArithmeticException.class);
+		l2.getSlope();
 	}
 	
 	@Test
 	public void getDistanceTest() {
 		Line l = new Line(1, 2, 3, 4);
-		assertEquals(2.8284, l.getDistance());
-		assertEquals(2.8284, l.getDistance());
+		assertEquals(2.8284, l.getDistance(), .0001);
+		assertNotEquals(5, l.getDistance(),.0001);
 	}
 	
 	@Test
 	public void parallelToTest() {
 		Line l = new Line(1, 2, 3, 4);
-		assertEquals(true, l.parallelTo(new Line(1, 2 , 4, 5)));		
-		assertEquals(false, l.parallelTo(new Line(1, 2 , 4, 5)));
+		assertEquals(true, l.parallelTo(new Line(1, 2 , 2, 3)));		
+		assertEquals(false, l.parallelTo(new Line(5, 1 , 7, 2)));
 	}
 
 }
